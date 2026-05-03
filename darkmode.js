cbe8313c531086b349d55147043c21d5f0a4f6d4
shadow <dellasania52@gmail.com>
2026-05-03 18:14:43 +0700

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,136 @@
+let darkmode = localStorage.getItem('darkmode')
+const togglemode = document.getElementById('togglemode')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+togglemode.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+const search = document.querySelector('.search');
+const cari = document.querySelector('.cari');
+const input = document.querySelector('.input');
+
+// cari.addEventListener('click', () => {
+//     search.classList.toggle('active');
+//     input.focus();
+// });
+
+document.addEventListener('click', (e) => {
+    if (!search.contains(e.target)) {
+        search.classList.remove('active');
+    }
+});
+
+input.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+
+        let keyword = input.value.trim();
+
+        if (keyword !== "") {
+            console.log("Keyword", keyword);
+        }
+
+        input.value = "";
+        search.classList.remove('active');
+    }
+});
+
+var namaError = document.getElementById('nama-error');
+var telError = document.getElementById('tel-error');
+var emailError = document.getElementById('email-error');
+var pesanError = document.getElementById('pesan-error');
+var subError = document.getElementById('sub-error');
+
+function namaValidasi() {
+    var nama = document.getElementById('namakontak').value;
+    if(nama.length == 0) {
+        namaError.innerHTML = '';
+        return false;
+    }
+    if(!nama.match(/^[A-Za-z]+(\s[A-Za-z]+)+$/)){
+        namaError.innerHTML = 'masukkan nama lengkap';
+        return false;
+    }
+    namaError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+    return true;
+}
+
+function telValidasi() {
+    var nomor = document.getElementById('telkontak').value;
+
+    if(nomor.length == 0) {
+        telError.innerHTML = 'nomor tidak valid';
+        return false;
+    }
+    if(nomor.length < 10 || nomor.length > 13) {
+        telError.innerHTML = 'nomor harus 10-13 digit';
+        return false;
+    }
+    if(!nomor.match(/^[0-9]{10,13}$/)){
+        telError.innerHTML = 'nomor harus 10-13 digit';
+        return false;
+    }
+
+    telError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+    return true;
+}
+
+function emailValidasi() {
+    var email = document.getElementById('emailkontak').value;
+
+    if(email.length == 0) {
+        emailError.innerHTML = 'email tidak valid';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'email tidak sesuai';
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+    return true;
+}
+
+function pesanValidasi() {
+    var pesan = document.getElementById('pesankontak').value;
+    var valid = 30;
+    var left = valid - pesan.length;
+
+    if(left>0){
+        pesanError.innerHTML = left + 'masukkan lebih banyak karakter';
+        return false;
+    }
+
+    pesanError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+    return true;
+}
+
+function validasiForm() {
+    if(!namaValidasi() || !telValidasi() || !emailValidasi() || !pesanValidasi()){
+        subError.style.display = 'block';
+        subError.innerHTML = 'Lengkapi data';
+        setTimeout(function(){subError.style.display = 'none'}, 3000);
+        return false;
+    }
+
+    alert("Pesan berhasil dikirim!");
+    document.querySelector("form").reset();
+    namaError.innerHTML = "";
+    telError.innerHTML = "";
+    emailError.innerHTML = "";
+    pesanError.innerHTML = "";
+    return false;
+}
